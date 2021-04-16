d0d8dc7c89f94918b03d8e43c2e334487b4e7043
Marcel Hauri <marcel.hauri@gmail.com>
2021-04-16 08:35:58 +0200

    [task] add matomo tracking

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
-import '../src/styles/globals.css'
+import React from "react";
+import App from "next/app";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import '../src/styles/globals.css'
+import { init } from "@socialgouv/matomo-next";
+
+const MATOMO_URL = process.env.PUBLIC_MATOMO_URL;
+const MATOMO_SITE_ID = process.env.PUBLIC_MATOMO_SITE_ID;
+
+class MyApp extends App {
+  componentDidMount() {
+    init({ url: MATOMO_URL, siteId: MATOMO_SITE_ID });
+  }
+  render() {
+    const { Component, pageProps } = this.props;
+    return <Component {...pageProps} />;
+  }
 }
 
 export default MyApp
--- src/components/posts.js
+++ src/components/posts.js
@@ -2,7 +2,7 @@ import PostCard from "./posts/card";
 
 const Posts = ({data}) => {
   return (
-    <main className="min-h-screen flex flex-col mx-auto max-w-3xl px-4 md:px-0 pt-10">
+    <main className="min-h-screen flex flex-col mx-auto max-w-3xl px-4 md:px-0 pt-2">
       <div className="border-b border-color mb-5">
         <h1 className="pb-0 mb-0">Personal Hacker News Reader</h1>
         <span className="pb-5 block text-gray-600 text-xs">By <a href="https://hauri.dev" className="text-accent hover:text-black dark:hover:text-gray-50">Marcel Hauri</a></span>
